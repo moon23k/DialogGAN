9a112936b4a2cb4bb5650777487be7450141b5b6
moon <d.moon23k@gmail.com>
2024-03-20 23:34:35 +0900

    Initialized

--- run.py
+++ run.py
@@ -1,39 +1,28 @@
-import os, json, argparse, torch
-from tqdm import tqdm
-from module.data import load_dataloader
-from module.model import load_models
-from module.train import Trainer, PreTrainer
-from module.test import Tester
-from transformers import (set_seed,
-                          T5TokenizerFast, 
-                          T5ForConditionalGeneration)
+import json, argparse, torch
+from utils import set_trainer, load_dataset
+from transformers import (
+    set_seed, AutoTokenizer, 
+    AutoModelForSequenceClassification, 
+)
+
 
 
 
 class Config(object):
-    def __init__(self, args):    
+    def __init__(self, strategy):
 
-        self.mode = args.mode
-        self.model_name = "t5-small"
-
-        self.clip = 1
-        self.lr = 5e-5
-        self.n_epochs = 10
-        self.batch_size = 16
-        self.iters_to_accumulate = 4
+        self.strategy = strategy        
+        self.mname = 'bert-base-uncased'
         
-        use_cuda = torch.cuda.is_available()
-        self.device_type = 'cuda' if use_cuda else 'cpu'
+        self.lr = 1e-5
+        self.n_epochs = 5
+        self.batch_size = 32
+        self.max_len = 512
+        self.num_labels = 4
 
-        if self.mode == 'inference':
-            self.device = torch.device('cpu')
-        else:
-            self.device = torch.device('cuda' if use_cuda else 'cpu')
+        self.ckpt = f"ckpt/{strategy}"
+        self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-        self.gen_ckpt = f'ckpt/{self.task}_generator.pt'
-        self.dis_ckpt = f'ckpt/{self.task}_discriminator.pt'
-        self.gen_pre_ckpt= f'ckpt/pre_{self.task}_generator.pt'
-        self.dis_pre_ckpt = f'ckpt/pre_{self.task}_discriminator.pt'
 
     def print_attr(self):
         for attribute, value in self.__dict__.items():
@@ -41,122 +30,55 @@ class Config(object):
 
 
 
-def inference(model, tokenizer):
-    model.eval()
-    print(f'--- Inference Process Started! ---')
-    print('[ Type "quit" on user input to stop the Process ]')
-    
-    while True:
-        input_seq = input('\nUser Input Sequence >> ').lower()
 
-        #End Condition
-        if input_seq == 'quit':
-            print('\n--- Inference Process has terminated! ---')
-            break        
+def main(strategy):
 
-        #convert user input_seq into model input_ids
-        input_ids = tokenizer(input_seq)['input_ids']
-        output_ids = model.generate(input_ids, beam_size=4, max_new_tokens=300, use_cache=True)
-        output_seq = tokenizer.decode(output_ids, skip_special_tokens=True)
-
-        #Search Output Sequence
-        print(f"Model Out Sequence >> {output_seq}")       
-
-
-def pretrain(config, generator, discriminator):
-    train_dataloader = load_dataloader(config, 'train')
-    valid_dataloader = load_dataloader(config, 'valid')
-    
-    model = generator if generator is not None else discriminator
-    pretrainer = PreTrainer(config, model, train_dataloader, valid_dataloader)
-    pretrainer.train()
-
-
-def train(config, generator, discriminator):
-    train_dataloader = load_dataloader(config, 'train')
-    valid_dataloader = load_dataloader(config, 'valid')
-
-    trainer = Trainer(config, generator, discriminator, train_dataloader, valid_dataloader)
-    trainer.train()
-
-
-def test(config, model, tokenizer):
-    test_dataloader = load_dataloader(config, 'test')
-    tester = Tester(config, model, tokenizer, test_dataloader)
-    tester.test()    
-
-
-def generate(config, model, split):
-    generated = []
-    dataloader = load_dataloader(config, split)
-
-    model.eval()
-    with torch.no_grad():
-        for _, batch in tqdm(enumerate(dataloader)):   
-            input_ids = batch[f'{config.src}_ids'].to(config.device)
-            attention_mask = batch[f'{config.src}_mask'].to(config.device)
-            labels = batch[f'{config.trg}_ids'].tolist()
-
-            with torch.autocast(device_type=config.device_type, dtype=torch.float16):
-                preds = model.generate(input_ids=input_ids, attention_mask=attention_mask, 
-                                       max_new_tokens=300, use_cache=True).tolist()
-            
-            for pos, neg in zip(labels, preds):
-                generated.append({'input_ids': [id for id in pos if id], 'labels': 1})
-                generated.append({'input_ids': [id for id in neg if id], 'labels': 0})
-
-    with open(f"data/dis_{split}.json", 'w') as f:
-        json.dump(generated, f)
-        assert os.path.exists(f'data/gen_{split}.json')
-
-
-def main(args):
+    #Prerequisites
     set_seed(42)
-    config = Config(args.task, args.task)    
-    generator, discriminator = load_models(config)
-    
-    if generator is not None:
-        setattr(config, 'pad_id', generator.config.pad_token_id)
-        generator.config.update({'decoder_start_token_id': config.pad_id})
-    else:
-        setattr(config, 'pad_id', discriminator.pad_id)
+    config = Config(strategy)
 
-    if config.task not in  ['pretrain','train']:
-        tokenizer = T5TokenizerFast.from_pretrained(config.model_name, model_max_length=300)
-
-    #Actual Processing Codes
-    if config.mode == 'pretrain':
-        pretrain(config, generator, discriminator)
-    elif config.mode == 'generate':
-        generate(config, generator, 'train')
-        generate(config, generator, 'valid')
-    elif config.mode == 'train':
-        train(config, generator, discriminator)
-    elif config.mode == 'test':
-        test(config, generator, tokenizer)
-    elif config.mode == 'inference':
-        inference(generator, tokenizer)
+    tokenizer = AutoTokenizer.from_pretrained(
+        config.mname, model_max_length=config.max_len
+    )
     
+    model = AutoModelForSequenceClassification.from_pretrained(
+        config.mname, num_labels=config.num_labels
+    ).to(config.device)
+
+    if config.strategy in ['grad_checkpointing', 'all']:
+        model.config.use_cache = False
+
+
+    #Load datasets
+    train_dataset = load_dataset(tokenizer, 'train')
+    valid_dataset = load_dataset(tokenizer, 'valid')
+    test_dataset = load_dataset(tokenizer, 'test')
+
+    #Load Trainer
+    trainer = set_trainer(config, model, tokenizer, train_dataset, valid_dataset)    
+    
+    #Training
+    torch.cuda.reset_max_memory_allocated()
+    train_output = trainer.train()
+    gpu_memory = torch.cuda.max_memory_allocated()
+    
+    #Evaluating
+    eval_output = trainer.evaluate(test_dataset)
+    
+    #Save Training and Evaluation Rst Report
+    report = {**train_output.metrics, **eval_output}
+    report['gpu_memory'] = f"{gpu_memory / (1024 ** 3):.2f} GB"
+    with open(f"report/{strategy}.json", 'w') as f:
+        json.dump(report, f)
+
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('-mode', required=True)
-    parser.add_argument('-model', default='generator', required=False)
-    
+    parser.add_argument('-strategy', required=True)
+
     args = parser.parse_args()
-    assert args.mode in ['pretrain', 'generate', 'train', 'test', 'inference']
+    assert args.strategy.lower() in ['vanilla', 'fp16', 'grad_accumulation', 
+                                     'grad_checkpointing', 'adafactor', 'all']
 
-
-    if args.mode == 'pretrain':
-        assert args.model in ['generator', 'discriminator']
-        if args.model == 'discriminator':
-            assert os.path.exists('data/samples.json')
-    else:
-        if args.mode == 'train':
-            assert os.path.exists(f'ckpt/pre_{args.task}_generator.pt')
-            assert os.path.exists(f'ckpt/pre_{args.task}_discriminator.pt')
-        else:
-            assert os.path.exists(f'ckpt/{args.task}_generator.pt')
-
-    main(args)
+    main(args.strategy)
--- setup.py
+++ setup.py
@@ -1,97 +1,66 @@
-import os, re, json 
+import os, re, json
 from datasets import load_dataset
-from transformers import T5TokenizerFast
 
 
 
-def preprocess_data(orig_data, volumn=36000):
+
+
+def process_data(data_volumn):
+    #load original dataset
+    nmt_data = load_dataset('wmt14', 'de-en', split='train')['translation']
+    
+    min_len = 10 
+    max_len = 300
+    max_diff = 50
     volumn_cnt = 0
-    uttr_list, resp_list, processed = [], [], []
+    processed = []
+    
+    for elem in nmt_data:
+        temp_dict = dict()
+        x, y = elem['en'].strip().lower(), elem['de'].strip().lower()
+        x_len, y_len = len(x), len(y)
 
-    for dial in orig_data:
-        dial_list = []
-        dial_turns = len(dial)
-        
-        for uttr in dial:
-            _uttr = re.sub(r"\s([?,.!’](?:\s|$))", r'\1', uttr)
-            _uttr = re.sub(r'([’])\s+', r'\1', _uttr)
-            dial_list.append(_uttr.strip().lower())
-        
-        if dial_turns < 2:
-            continue
+        #Filtering Conditions
+        min_condition = (x_len >= min_len) & (y_len >= min_len)
+        max_condition = (x_len <= max_len) & (y_len <= max_len)
+        dif_condition = abs(x_len - y_len) < max_diff
 
-        elif dial_turns == 2:
-            uttr_list.append(dial_list[0])
-            resp_list.append(dial_list[1])
-            continue  #To avoid duplicate on below condition
-
-        #Incase of dial_turns is even
-        elif dial_turns % 2 == 0:
-            uttr_list.extend(dial_list[0::2])
-            resp_list.extend(dial_list[1::2])
-
-            uttr_list.extend(dial_list[1:-1:2])
-            resp_list.extend(dial_list[2::2])
-        
-        #Incase of dial_turns is odds
-        elif dial_turns % 2 == 1:
-            uttr_list.extend(dial_list[0:-1:2])
-            resp_list.extend(dial_list[1::2])
+        if max_condition & min_condition & dif_condition:
+            processed.append({'x': x, 'y':y})
             
-            uttr_list.extend(dial_list[1::2])
-            resp_list.extend(dial_list[2::2])   
-
-    assert len(uttr_list) == len(resp_list)
-    for uttr, resp in zip(uttr_list, resp_list):
-        temp_dict = dict()
-        temp_dict['uttr'] = uttr
-        temp_dict['resp'] = resp
-        processed.append(temp_dict)
-
-        #End Condition
-        volumn_cnt += 1
-        if volumn_cnt == volumn:
-            break
-    
-    return processed
+            #End condition
+            volumn_cnt += 1
+            if volumn_cnt == data_volumn:
+                break
 
 
+    return processed 
 
-def tokenize_data(tokenized, tokenizer):
-    tokenized_data = []
-    for elem in tokenized:
 
-        temp_dict = dict()
-        encodings = tokenizer(elem['uttr'], truncation=True)
-
-        temp_dict['input_ids'] = encodings.input_ids
-        temp_dict['attention_mask'] = encodings.attention_mask
-        temp_dict['labels'] = tokenizer.encode(elem['resp'], truncation=True)
-
-        tokenized_data.append(temp_dict)
-    
-    return tokenized_data
 
 
 def save_data(data_obj):
     #split data into train/valid/test sets
-    train, valid, test = data_obj[:-6000], data_obj[-6000:-3000], data_obj[-3000:]
+    train, valid, test = data_obj[:-5100], data_obj[-5100:-100], data_obj[-100:]
     data_dict = {k:v for k, v in zip(['train', 'valid', 'test'], [train, valid, test])}
 
     for key, val in data_dict.items():
         with open(f'data/{key}.json', 'w') as f:
             json.dump(val, f)        
         assert os.path.exists(f'data/{key}.json')
-    
+
+
+
 
 def main():
-    orig = load_dataset('daily_dialog', split='train')['dialog']
-    processed = preprocess_data(orig)
+    #PreProcess Data
+    data_volumn = 55100
+    processed = process_data(data_volumn)
 
-    tokenizer = T5TokenizerFast.from_pretrained("t5-small")
-    tokenized = tokenize_data(processed, tokenizer)
-    save_data(tokenized)
+    #Save Data
+    save_data(task, processed)
+
 
 
 if __name__ == '__main__':
-    main()
+    main()    
